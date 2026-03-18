c0bc667fd0d76bf112653156853860b0000bbe7c
Bogdane Braoun <112286@ecole-it.com>
2026-03-18 10:17:54 +0100

    add basic pages system - structure

---
+++ public/mentions.php
@@ -0,0 +1,36 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mentions légales</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+    
+    <header>
+
+        <?php require_once("./assets/modules/header.php");   ?>
+
+    </header>
+
+    <section class = "main_bck">
+        <h1>Mentions légales</h1>
+        <p>
+            Éditeur du siteNom : Bogdane BraounEmail : 112286@ecole-it.com
+            HébergementHébergeur : InfinityFreeSite : https://infinityfree.net
+            Responsable de publicationL’Atelier du Barbier
+            Propriété intellectuelleLe contenu du site (textes, images, code) est protégé. Toute reproduction est interdite sans autorisation.
+            ResponsabilitéL’éditeur ne peut être tenu responsable des erreurs ou indisponibilités du site.
+            Données personnellesLes données collectées via le formulaire (nom, email, téléphone) sont utilisées uniquement pour la gestion des réservations.Aucune donnée n’est vendue ou partagée.
+            Vous pouvez demander la suppression de vos données à : 112286@ecole-it.com
+        </p>
+    </section>
+
+    <footer> 
+
+        <?php require_once("./assets/modules/footer.php");   ?> 
+
+    </footer>
