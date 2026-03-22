35b3c904fc6ec16f764e18dbcda7ed575810e9c6
4h4ze <4h4ze@proton.me>
2026-03-22 17:28:13 +0100

    Front-end -Final

--- public/assets/modules/header.php
+++ public/assets/modules/header.php
@@ -3,14 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap" rel="stylesheet">
+    <title>Document</title>
 </head>
 <body>
 
     <div id = "header">
 
-        <img class="logo_nav" src="assets/images/logo_navbar.png" alt=" logo de la navbar">
+        <img class="logo_nav" src="assets/images/logo_nav.png" alt=" logo de la navbar">
 
         <div class= "nav_link">
             <a href="index.php"> Accueil </a>
